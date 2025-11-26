14af19449ee755aae8e2223175d6b4952e93545f
Miriama Mattová <miriama.mattova@scalesoft.cz>
2025-11-26 09:04:19 +0100

    simplyfy

--- src/main.js
+++ src/main.js
@@ -1,18 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import { createRouter, createWebHistory } from "vue-router";
-import Login from "./pages/Login.vue";
-import Home from "./pages/Home.vue";
+import './assets/main.css'
 
-const routes = [
-    { path: "/", component: Home },
-    { path: "/login", component: Login },
-    // optionally: API route /api/protected if you implement server
-];
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes,
-});
-
-createApp(App).use(router).mount("#app");
+createApp(App).mount('#app')
