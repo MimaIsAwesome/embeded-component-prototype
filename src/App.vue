e7a5a093c3bee240457a7a1fbb225fcd8d70c24c
Miriama Mattová <miriama.mattova@scalesoft.cz>
2025-11-26 09:02:51 +0100

    simplyfy

--- src/App.vue
+++
@@ -1,7 +0,0 @@
-﻿<template>
-  <router-view />
-</template>
-
-<script>
-export default {};
-</script>
--- src/pages/Home.vue
+++
@@ -1,7 +0,0 @@
-﻿<template>
-  <div style="padding:20px">
-    <h2>App B (Provider simulation)</h2>
-    <p>This is App B's home. Use /login to simulate the popup OIDC flow.</p>
-    <p><a href="/login" target="_self">Open Login Page</a></p>
-  </div>
-</template>
